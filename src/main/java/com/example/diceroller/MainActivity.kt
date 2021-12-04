531d1f8ddef6fe0f49b9fa7376c6ee85deb70906
larten13 <ya.dimanys@mail.ru>
2021-12-04 18:00:45 +0400

    кубик!

---
+++ src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * This method is called when the Activity is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the Button in the layout
+        val rollButton: Button = findViewById(R.id.button)
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+
+        // Update the screen with the dice roll
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Выбор ресурса drawable в зависимости от выпавшего числа
+            val drawableResource = when (dice.roll()) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        // Присвоение ресурса drawable в зависимости от выпавшего числа
+        diceImage.setImageResource(drawableResource)
+
+        // Описание для drawable (для огранич. возможностей)
+        diceImage.contentDescription = drawableResource.toString()
+
+    }
+}
+
+/**
+ * Dice with a fixed number of sides.
+ */
+class Dice(private val numSides: Int) {
+
+    /**
+     * Do a random dice roll and return the result.
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
